1c668a16d6d09a0193541f22517491086356696b
Gamze_Nur_Aslan <150529563+GamzeNurAslan@users.noreply.github.com>
2024-04-23 13:56:30 +0300

    var and val

---
+++ Main.kt
@@ -0,0 +1,15 @@
+//varandval
+fun main() {
+    val dress = 350
+    //dress = 200 => ERROR
+    val shirtlength= 1.96
+    val trousers = "wide leg"
+    println(dress)
+    println(shirtlength)
+    println(trousers)
+}
+/*Output:
+350
+1.96
+wide leg
+ */
